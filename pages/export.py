aa2cef537f7334cce4a4c5b2c7f686bf6949fe2f
ioul10 <ioulmadani@gmail.com>
2026-03-17 11:48:12 +0000

    Update export.py

--- pages/export.py
+++ pages/export.py
@@ -204,7 +204,7 @@ def generate_report_html():
         """
     
     # SECTION 5 : NEWS
-    if 'news' in selected and news_
+    if 'news' in selected and news_data:
         html += """
             <div class="section">
                 <h2>📰 Actualités Marquantes</h2>
